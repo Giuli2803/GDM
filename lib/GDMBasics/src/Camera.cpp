dc22cf801cee48af20d6ed33fc9fb66548ea1ac2
EllySparky <sparky.pizza.427@gmail.com>
2024-08-13 17:31:21 -0300

    - Modify Camera::loop() to use ord_sprite < operator

--- lib/GDMBasics/src/Camera.cpp
+++ lib/GDMBasics/src/Camera.cpp
@@ -67,7 +67,7 @@ void Camera::renderLoop()
         auto spt_b = b.lock();
         int depth_a = spt_a->getElementDepth();
         int depth_b = spt_b->getElementDepth();
-        return (depth_a < depth_b || (depth_a == depth_b && spt_a->getSpriteDepth() < spt_b->getSpriteDepth()));
+        return (depth_a < depth_b || (depth_a == depth_b && spt_a->getSprite() < spt_b->getSprite()));
     });
 
     for (const auto &sprite : _visible_sprites)
