3184fd150ceb89b130d9c5da9bfd6b50f166cd47
EllySparky <sparky.pizza.427@gmail.com>
2024-08-16 12:39:52 -0300

    - Trigger rework
    * Triggers where keept on a TriggerManager instance on Game, this would make them persist tru Rooms unless a manual cleaning was performed.
    * To fix that issue Game doesn't have a TriggerManager anymore, instead each Room has an instance of TriggerManager to keep track of it's contained Triggers.
    * Trigger now inherit from Element, this makes Trigger::offset useless since the Trigger itself has a LocalCoords independent of any other object.
    * For that reson Trigger::offset and it's related methods where removed.
    * The classes Declarations on GDMBasics where moved arround since now Room requires TriggerManager's declaration and Trigger requires Element's declaration.
    * TriggerManager::checkTrigger() was modified to use Trigger::getWorldPosition() and Trigger::getWorldScale()

--- lib/GDMBasics/src/Trigger.cpp
+++ lib/GDMBasics/src/Trigger.cpp
@@ -5,24 +5,5 @@
 
 namespace mate
 {
-sf::Vector2f Trigger::getPosition() const
-{
-    sf::Vector2f reference(0, 0);
-    if (std::shared_ptr<Element> spt_parent = _follow.lock())
-    {
-        reference = spt_parent->getWorldPosition();
-    }
-    return _offset.getPositionBounds(reference);
-}
-
-sf::Vector2f Trigger::getDimensions() const
-{
-    sf::Vector2f reference(1, 1);
-    if (std::shared_ptr<Element> spt_parent = _follow.lock())
-    {
-        reference = spt_parent->getWorldScale();
-    }
-    return _offset.getDimensionBounds(reference);
-}
 
 } // namespace mate
